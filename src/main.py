36194c43e7d79b25b5e0d8a04031d2aae370ce63
twopats <lpatipat@icloud.com>
2023-08-20 22:50:48 -0400

    SETUP: basic sk set up

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import semantic_kernel as sk
+import os
 from semantic_kernel.connectors.ai.open_ai import OpenAITextCompletion
+from dotenv import dotenv_values 
+
+skills_dir = "./src/skills"
+
+API_KEY = dotenv_values(".env")["OPENAI_API_KEY"]
 
 kernel = sk.Kernel()
 
 
-api_key = sk.openai_settings_from_dot_env()
 
 kernel = sk.Kernel()
 
@@ -12,7 +17,13 @@ kernel.add_text_completion_service(               # We are adding a text service
     "OpenAI_davinci",                         # The alias we can use in prompt templates' config.json
     OpenAITextCompletion(
         "text-davinci-003",                   # OpenAI Model Name
-        api_key
+        API_KEY
     )
 )
 
+
+
+jokeFunction = kernel.import_semantic_skill_from_directory(skills_dir, "FunSkill")["Joke"]
+
+result = jokeFunction("the weather in new york city")
+print(result)
